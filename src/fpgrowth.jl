3987653e02e01c0c7fd79ed44694857775e3178e
DopedCow <oerris@me.com>
2022-07-08 08:22:19 +0200

    Create fpgrowth.jl

---
+++ src/fpgrowth.jl
@@ -0,0 +1,8 @@
+"""
+    fpgrowth(transactions, support, confidence, lift)
+
+Returns a set of rules found in the transactions. If values for support, confidence or lift are provided, only rules that are above those thresholds will be included.
+"""
+function fpgrowth(transactions, support, confidence, lift)
+  return rules
+end
