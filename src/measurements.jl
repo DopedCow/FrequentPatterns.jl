aa38b0f97cea0c739812cb0c26c5b24942767aee
Morten Ørris Poulsen <oerris@me.com>
2023-08-29 21:24:29 +0200

    Moved measurements out of eclat.jl

--- src/measurements.jl
+++ src/measurements.jl
@@ -5,8 +5,19 @@ end
 
 """
 """
-function confidence()
+function confidence(rules, supp, minimum_confidence = 0)
+    confidence = Dict()
+
+    for k in keys(rules)
+        for s in powerset(k, 2, 3)
+            println(s)
+        end
+        c = rules[k] / supp[k[1]]
+        c >= minimum_confidence && push!(confidence, k => c)
+    end
+    return confidence
 end
+
 
 """
 """
